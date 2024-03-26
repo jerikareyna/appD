7c13f8b5f9952f4dc34ccd87e8903707bbe1e79b
jerikareyna <105334298+jerikareyna@users.noreply.github.com>
2024-03-25 21:28:59 -0500

    Mi primer commit

---
+++ jeri/src/main/java/smart/logical/jeri/JeriApplication.java
@@ -0,0 +1,13 @@
+package smart.logical.jeri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeriApplication.class, args);
+	}
+
+}
---
+++ jeri/src/test/java/smart/logical/jeri/JeriApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.logical.jeri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
